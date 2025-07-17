919eec983b9edb9c7bbaf5037bd94592b93ee03a
Ksenia-Schuklina <ksusha.shuklina.5@gmail.com>
2025-07-17 11:31:44 +0300

    The first commit: add the project of telegram bot

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = '7258331431:AAEyQGnKCqMDmm30lBOnzrV4fiJ5W8IF5L8'
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
+    
