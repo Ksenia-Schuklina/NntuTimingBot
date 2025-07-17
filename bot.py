ce64ea0767ea8ea974cd21a64bd431ddfbfa2d2c
Ksenia <ksusha.shuklina.5@gmail.com>
2025-07-17 14:38:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = '7258331431:AAEyQGnKCqMDmm30lBOnzrV4fiJ5W8IF5L8'
+TOKEN = ''
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
